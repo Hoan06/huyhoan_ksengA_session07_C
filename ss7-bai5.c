c9f2b0935c9b9a1a534b2469996857a1fc77693f
Hoan06 <167394101+Hoan06@users.noreply.github.com>
2024-11-27 14:51:15 +0700

    btvn_ss7_bai5

--- ss7-bai5.c
+++ ss7-bai5.c
@@ -2,15 +2,19 @@
 
 int main(){
 	
-	int max, number[5]={1,3,5,2,7};
+	int max,min, number[5]={1,3,5,2,7};
 	max=number[1];
+	min=number[7];
 	for(int i=0;i<5;i++){
 		if(number[i]>max){
 			max=number[i];
 		}
+		if(number[i]<min){
+			min=number[i];
+		}
 	}
-	printf("%d",max);
-	
+	printf("sln %d\n",max);
+	printf("snn %d",min);
 	
 	return 0;
 }
