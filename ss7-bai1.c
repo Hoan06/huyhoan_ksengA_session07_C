0d6a94903bccf277753a28e63977f9460fb40426
Hoan06 <167394101+Hoan06@users.noreply.github.com>
2024-11-27 08:20:28 +0700

    btvn_ss7_bai1

---
+++ ss7-bai1.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+  int main() {
+	
+	int arrayNum[5]={1,2,3,4,5};
+	
+	for (int i=0;i<5;i++){
+		printf("gia tri cua mang thu %d la %d\n",i,arrayNum[i]);
+	}
+	
+	
+	return 0;
+}
+
